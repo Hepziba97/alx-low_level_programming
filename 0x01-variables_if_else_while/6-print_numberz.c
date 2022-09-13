58e8f9c5f73924702680f6b08d17c7e71040b11a
Francis <francisdare740@gmail.com>
2022-09-12 18:57:58 -0700

    print lowercase alphabet and single digits number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,16 @@
 
 /**
  * main - main function
- * Description: print all single numbers below 10
+ * Description: print all single numbers of base 10 starting from 0
  * Return: 0 (Success
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 0; n < 10; n++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(n);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - prints all the numbers of base 16 in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, ch;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
